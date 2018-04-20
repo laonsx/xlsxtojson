8e313254da72bdf5c39a6305caa500098ef9229b
laonsx <zhang13652103006@163.com>
2018-04-21 00:02:35 +0800

    add flag function and example

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import (
-	"github.com/laonsx/xlsxtojson/export"
-)
+import "flag"
 
 func main() {
 
-	export.Run()
+	var fileDir, outDir string
+
+	flag.StringVar(&fileDir, "f", "./", "File need to export.")
+	flag.StringVar(&outDir, "o", "./json", "Output destination.")
+
+	flag.Parse()
+
+	files, _ := listDir(fileDir, "xlsx")
+	for _, f := range files {
+
+		doExportFile(f, outDir)
+	}
 }
